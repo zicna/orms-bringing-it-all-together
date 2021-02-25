ccac4595aa554f63da2bf4e14ee7f34892c888c0
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-02-25 13:02:47 -0600

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog
     attr_accessor :name, :breed, :id
 
-    def initialize(id: id, name: name, breed: breed)
+    def initialize(id: nil, name: , breed:)
         @id = id
         @name = name
         @breed = breed
